1bd73405348b9884f6b61692a714181d25a215ae
zahirul <zahirulopel@gmail.com>
2024-06-22 20:32:34 +0600

    Add form input types

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import "./App.css";
 import formData from "../Utils/FormData/formData";
 import FormInput from "./Components/FormInput";
+import { useState } from "react";
 
 function App() {
+  const [formValue, setFormValue] = useState({
+    fname: "",
+    lname: "",
+    mail: "",
+    query: "",
+    message: "",
+    agree: false,
+  });
+
+  const handleInputData = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
+
+  const handleCheckBox = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.checked });
+  };
+
+  const handleformSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValue);
+  };
   return (
     <>
       <main>
-        <form>
+        <form onSubmit={handleformSubmit}>
           <legend>Contact Us</legend>
           {formData.map((input) => (
             <FormInput
@@ -14,24 +36,54 @@ function App() {
               {...input}
               label={input.lable}
               id={input.name}
+              value={formValue[input.name]}
+              onChange={handleInputData}
             />
           ))}
           <div>
-            <label htmlFor="">Query Type</label>
-            <input type="radio" name="" id="" />
-            <label htmlFor="">General Enquiry</label>
+            <p>Query Type</p>
+            <input
+              type="radio"
+              name="query"
+              id="generalQuery"
+              value="general"
+              onChange={handleInputData}
+              checked={formValue["query"] === "general"}
+            />
+            <label htmlFor="generalQuery">General Enquiry</label>
 
-            <input type="radio" name="" id="" />
-            <label htmlFor="">Support Request</label>
+            <input
+              type="radio"
+              name="query"
+              id="supportQuery"
+              value="support"
+              onChange={handleInputData}
+              checked={formValue["query"] === "support"}
+            />
+            <label htmlFor="supportQuery">Support Request</label>
           </div>
           <div>
-            <label htmlFor="">Message</label>
-            <textarea name="" id=""></textarea>
+            <label htmlFor="msg">Message</label>
+            <textarea
+              name="message"
+              id="msg"
+              value={formValue["message"]}
+              onChange={handleInputData}
+            ></textarea>
           </div>
           <div>
-            <input type="checkbox" name="" id="" />
-            <label htmlFor="">I consent to being contacted by the team</label>
+            <input
+              type="checkbox"
+              name="agree"
+              id="agreed"
+              checked={formValue["agree"]}
+              onChange={handleCheckBox}
+            />
+            <label htmlFor="agreed">
+              I consent to being contacted by the team
+            </label>
           </div>
+          <button type="submit">Submit</button>
         </form>
       </main>
     </>
